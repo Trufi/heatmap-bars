519aa08ca45ea278349d4312b42c511f7e741de5
Trufi <stevemyz@gmail.com>
2022-02-14 16:12:35 +0000

    Deploying to gh-pages from @ Trufi/heatmap@113392b054007f6f5d213ebdf4317e89ced41381 🚀

--- module/animation.d.ts
+++
@@ -1,9 +0,0 @@
-export interface Animation {
-    startTime: number;
-    endTime: number;
-    from: number;
-    to: number;
-    value: number;
-}
-export declare const startAnimation: (anim: Animation, value: number, duration: number) => void;
-export declare const updateAnimation: (anim: Animation) => boolean;
